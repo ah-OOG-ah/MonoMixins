310b74f5dfb1cd7dbe92d1c957978a62eaaca088
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-08-15 14:35:43 -0400

    Import MixinBooterLegacy

--- src/main/java/io/github/legacymoddingmc/unimixins/all/AllCore.java
+++ src/main/java/io/github/legacymoddingmc/unimixins/all/AllCore.java
@@ -4,6 +4,7 @@ import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 import io.github.legacymoddingmc.unimixins.compat.CompatCore;
 import io.github.legacymoddingmc.unimixins.mixin.MixinCore;
+import io.github.tox1cozz.mixinbooterlegacy.MixinBooterLegacyPlugin;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,21 +19,22 @@ import org.apache.logging.log4j.Logger;
 @IFMLLoadingPlugin.TransformerExclusions({
         "io.github.legacymoddingmc.unimixins.compat.MixinCore",
         "io.github.legacymoddingmc.unimixins.compat.CompatCore",
-        "io.github.legacymoddingmc.unimixins.compat.asm"
+        "io.github.legacymoddingmc.unimixins.compat.asm",
+        "io.github.tox1cozz.mixinbooterlegacy.MixinBooterLegacyPlugin"
 })
 public class AllCore implements IFMLLoadingPlugin {
 
     public static final Logger LOGGER = LogManager.getLogger("unimixins");
 
     private static String[] embeddedCorePluginClassNames = new String[] {
-//            "io.github.tox1cozz.mixinbooterlegacy.MixinBooterLegacyPlugin",
 //            "com.gtnewhorizon.gtnhmixins.core.GTNHMixinsCore",
 //            "io.github.legacymoddingmc.unimixins.mixinextras.MixinExtrasCore"
     };
 
     private static final Class<?>[] knownCLaxxes = new Class[] {
         MixinCore.class,
-        CompatCore.class
+        CompatCore.class,
+        MixinBooterLegacyPlugin.class
     };
     private static final List<Class<?>> embeddedCorePluginClasses = new ArrayList<>(Arrays.asList(knownCLaxxes));
 
---
+++ src/main/java/io/github/tox1cozz/mixinextras/MixinExtrasAnnotationProcessor.java
@@ -0,0 +1,35 @@
+package io.github.tox1cozz.mixinextras;
+
+import java.util.Set;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import org.spongepowered.asm.util.logging.MessageRouter;
+
+@SupportedAnnotationTypes({})
+public class MixinExtrasAnnotationProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        try {
+            MessageRouter.setMessager(processingEnv.getMessager());
+            MixinExtrasBootstrap.init();
+        } catch (NoClassDefFoundError e) {
+            // The Mixin AP probably isn't available, e.g. if loom has excluded it from IDEA.
+        }
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+}
